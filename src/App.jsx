529b1b921db001af220217cccda9ba1d6dd1f030
rmarttor <rmarttor@emeal.nttdata.com>
2023-08-17 13:44:42 +0200

    cambio 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useEffect, userState } from 'react'
-const FILM_ENDPOINT_RANDOM_FACT = 'http://www.omdbapi.com/?apikey=[yourkey]&'
+import { useEffect, useState } from 'react'
+// const FILM_ENDPOINT_RANDOM_FACT = 'http://www.omdbapi.com/?apikey=[cc8f29a0]&'
 
 export function App () {
-  const [fact, setFact] = userState()
+  const [fact, setFact] = useState()
+  const title = 'Matrix'
 
   useEffect(() => {
-    fetch(FILM_ENDPOINT_RANDOM_FACT)
+    // TODO: Las api keys van en el fichero de configuración .env
+    fetch(`http://www.omdbapi.com/?apikey=cc8f29a0&t=${title}`)
       .then(res => res.json())
       .then(data => setFact(data.fact))
   }, [])
